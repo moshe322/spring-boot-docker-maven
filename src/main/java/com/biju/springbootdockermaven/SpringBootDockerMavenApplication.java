d59f1b5f81d4083966c1cf351da47cd413c2b783
Bijendra2022 <harekrishna.soft@gmail.com>
2022-10-09 14:00:13 +0530

    first commit

---
+++ src/main/java/com/biju/springbootdockermaven/SpringBootDockerMavenApplication.java
@@ -0,0 +1,13 @@
+package com.biju.springbootdockermaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerMavenApplication.class, args);
+	}
+
+}
